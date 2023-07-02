33aa4020a7e8dbc42efdfd48678e25c3256dd966
Mr. T <89462237+EndlessFractal@users.noreply.github.com>
2023-07-02 11:14:30 +0100

    Update upd_hosts.py
    - Clearer variable naming.
    - Now using f-strings for string formatting.
    - Now using list comprehension for line filtering and processing, making the code more concise.
    - The requests.get call now uses *with* to ensure proper release of network resources.

--- upd_hosts.py
+++ upd_hosts.py
@@ -6,45 +6,40 @@ from datetime import datetime
 with open("list.txt", "r") as file:
     urls = file.read().splitlines()
 
-filtered_hosts = set()
-
 # Fetch and process host files from each URL
+unique_domains = set()
 for url in urls:
-    response = requests.get(url)
-    lines = response.text.splitlines()
-    for line in lines:
-        line = re.sub(r"\s+#.*$", "", line)
-        line = line.strip().replace("||", "").replace("^", "")
-        if line and not line.startswith(("#", "::1")):
-            domain = re.sub(r"^(?:\d{1,3}\.){3}\d{1,3} ", "", line).strip().lower()
-            if "." in domain:
-                filtered_hosts.add(domain)
+    with requests.get(url) as response:
+        lines = response.text.splitlines()
+        lines = [re.sub(r"\s+#.*$", "", line) for line in lines]
+        lines = [line.strip().replace("||", "").replace("^", "") for line in lines if line and not line.startswith(("#", "::1"))]
+        domains = [re.sub(r"^(?:\d{1,3}\.){3}\d{1,3} ", "", line).strip().lower() for line in lines if "." in line]
+        unique_domains.update(domains)
 
-# Sort the filtered hosts alphabetically
-filtered_hosts = sorted(filtered_hosts)
+# Sort the unique domains alphabetically
+unique_domains = sorted(unique_domains)
 
 # Get current date and time
 now = datetime.now()
 last_update = now.strftime("%d %B %Y %H:%M:%S (UTC)")
 
 # Get the number of unique domains
-num_domains = len(filtered_hosts)
+num_domains = len(unique_domains)
 
-# Create the header for the output file
-header = """\
+# Create the header for the output file using f-string
+header = f"""\
 # Title: EndlessFractal/hosts
 #
-# Date of Last Update: {}
-# Number of Unique Domains: {}
+# Date of Last Update: {last_update}
+# Number of Unique Domains: {num_domains}
 #
 # Fetch the latest version of this file: https://raw.githubusercontent.com/EndlessFractal/hosts/main/hosts.txt
 # Project home page: https://github.com/EndlessFractal/hosts
 #
 # ===============================================================
-""".format(last_update, num_domains)
+"""
 
 # Write the filtered hosts to a file
 with open("hosts.txt", "w") as file:
     file.write(header)
-    for domain in filtered_hosts:
-        file.write(domain + "\n")
+    file.writelines(domain + "\n" for domain in unique_domains)
